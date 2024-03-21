5eeba1d73791c06b5fe3e6e731a3858afabf5073
John Lopez <john.lopez3695@gmail.com>
2024-03-21 15:06:50 -0400

    updated logic for edit and deletion of user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,8 +7,8 @@ const router = Router();
 router.post("/register", controllers.registerUser); //Will create a new user in the register endpoint
 router.post("/login", controllers.login); //Will log user in
 router.get("/verify", controllers.verify); //will verify user with their unique token
-router.put("/edit" , controllers.edit);
-router.delete("/edit", controllers.deleteUser);
+router.put("/edit", controllers.edit);
+router.delete("/delete", controllers.deleteUser);
 // router.post("/change-password", controllers.changePassword); //Will allow user to change their password
 
 export default router;
