58ebcb609e485bd4fdeba5ee8cf3f8c84a865af2
John Lopez <john.lopez3695@gmail.com>
2024-03-21 15:08:05 -0400

    refactored code

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+
 /* CONFIGURATIONS */
 dotenv.config();
 // for production
